26ec70439d018cfa3f17ebe3b0f48be4459d5848
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-11-08 14:55:59 +0000

    exercise 1

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
-"""Forms of the project."""
+from django import forms
+from .models  import Thing
 
-# Create your forms here.
+class ThingForm():
+    name = forms.CharField(label='name')
+    description = forms.charField(
+        label = 'description'
+        widget={forms.Textarea()}
+    )
+    quantity = forms.NumberInput(
+        label = 'quantity'
+    )
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .forms import ThingForm
 
 # Create your tests here.
+
+class ThingFormTestCase(TestCase):
+    
+    def test_valid_sign_up_form(self):
+        self.form_input = {
+            'name' : 'Jaeho',
+            'description' : 'This is Jaeho signing up',
+            'quantity' : '3'
+        }
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+        self.assertFalse(form.is_invalid())
